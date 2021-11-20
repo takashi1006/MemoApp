89c8fe0f4029b93e117e531bc5a3bc95416821e8
takashi1006 <takashi1006@live.jp>
2021-11-20 14:47:05 +0900

    install ESLint

--- App.jsx
+++ App.jsx
@@ -2,14 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+let App;
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable */}
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
