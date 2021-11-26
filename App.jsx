98240139ff6df5c599b2b1c9f72336b126e80e7b
takashi1006 <takashi1006@live.jp>
2021-11-26 12:48:26 +0900

    create memo create/edit screen

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScr÷een from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function app() {
   return (
-    <MemoListScreen />
+    <MemoCreateScreen />
   );
 }
